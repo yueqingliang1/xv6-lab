1516843f2e02880e183f36050a4e9d9a997721cf
yueqingliang1 <yueqingliang1@gmail.com>
2022-02-28 23:19:37 -0600

    add trace_mask = 0 to freeproc() in proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -164,6 +164,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->trace_mask = 0; //每次reset trace_mask
 }
 
 // Create a user page table for a given process,
