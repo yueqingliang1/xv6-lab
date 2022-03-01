e48a9f27e8150ce282b32e31a45a02b632704808
yueqingliang1 <yueqingliang1@gmail.com>
2022-02-28 23:26:23 -0600

    modify fork() in kernel/proc.c to copy the trace_mask from the parent to the child process.

--- kernel/proc.c
+++ kernel/proc.c
@@ -296,6 +296,9 @@ fork(void)
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
+  // copy trace_mask in the child
+  np->trace_mask = p->trace_mask;
+
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
     if(p->ofile[i])
